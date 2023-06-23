dc329ce85d348d985490a1ab8550f7d5a25c226e
Laxr24 <73450829+Laxr24@users.noreply.github.com>
2023-06-23 22:34:23 +0600

    "Quit" alert added!
    Implemented onBackPressed method to show "Quit" alert dialog

--- app/src/main/java/com/example/fakecaller/MainActivity.java
+++ app/src/main/java/com/example/fakecaller/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.fakecaller;
 
 import android.app.ActionBar;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -51,5 +53,18 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this)
+                .setTitle("Want to quit?")
+                .setPositiveButton("Yes", (dialog1, which) -> {
+                    super.onBackPressed();
+                })
+                .setNegativeButton("No", ((dialog1, which) -> {
+
+                }));
+        dialog.show();
     }
 }
